c2607d532b7764b671911e404f9be86a304df4dc
nikhiltagare <nikhiltagare9@gmail.com>
2025-04-04 09:34:41 +0530

    intial commit

--- src/main/java/WebPageText.java
+++ src/main/java/WebPageText.java
@@ -1,2 +1,38 @@
-public class WebPageText {
+package org.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class WebPageTest {
+    private static WebDriver driver;
+
+    @BeforeTest
+    public void openBrowser() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        driver.get("");
+    }
+
+    @Test
+    public void titleValidationTest(){
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Tripillar Solutions";
+        Assert.assertEquals(actualTitle, expectedTitle);
+
+    }
+
+    @AfterTest
+    public void closeBrowser() throws InterruptedException {
+        Thread.sleep(1000);
+        driver.quit();
+    }
+
+
 }
